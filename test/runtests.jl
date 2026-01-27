b6cda9fd8d90836aa31e1b6a58de5fcabfa9c59f
Dvegrod <dvegrodu@gmail.com>
2026-01-27 17:44:34 +0100

    Add rm to clean after tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,6 @@ if !(PerfTest.Configuration.load_config() isa Nothing)
     include("t2-validation-macro.jl")
     include("t3-transforms.jl")
 
+    rm(".perftest_logs", recursive=true, force=true)
+    rm(".perftests", recursive=true,force=true)
 end
